bad2529815a7510255152c76da1a08667530b510
Psyrens <72940171+Psyrens@users.noreply.github.com>
2020-10-19 15:12:40 +0330

    Add files via upload

---
+++ pyirc.py
@@ -0,0 +1,57 @@
+from socket import *
+
+def hirc():
+	username = input("username: ")
+	ip = input("ip: ")
+	port = input("port: ")
+	s = socket(2, 1)
+	try:
+		s.bind((str(ip), int(port)))
+		s.listen(10)
+		print("listening on " + str(ip) + " through port " + str(port))
+		c, addr = s.accept()
+		print("connected to " + str(addr[0]) + " through port " + str(addr[1]))
+		while True:
+			try:
+				message = input(str(username) + " #> ")
+				c.sendall(bytes(username + " #> " + message, encoding = 'utf-8'))
+				data = c.recv(5120)
+				print(data.decode("utf-8"))
+			except:
+				print("could not recv/send data to " + str(addr[0]) + " through port " + str(addr[1]))
+	except:
+		print("could not bind on " + str(ip) + ":" + str(port))
+
+	s.close()
+
+def circ():
+	username = input("username: ")
+	ip = input("ip: ")
+	port = input("port: ")
+	s = socket(2, 1)
+	try:
+		s.connect((str(ip), int(port)))
+		print("connected to " + str(ip) + " through port " + str(port))
+		while True:
+			try:
+				data = s.recv(5120)
+				print(data.decode("utf-8"))
+				message = input(str(username) + " #> ")
+				s.sendall(bytes(username + " #> " + message, encoding = 'utf-8'))
+			except:
+				print("could not recv/send data to " + str(ip) + " through port " + str(port))
+	except:
+		print("could not connect to " + str(ip) + " through port " + str(port))
+		
+	s.close()
+
+while True:
+	choc = str(input("[1] Host\n[2] Connect\n[0] Exit\n"))
+	if choc == '1':
+		hirc()
+	elif choc == '2':
+		circ()
+	elif choc == '0':
+		exit(0)
+	else:
+		print("wrong input, try again")
